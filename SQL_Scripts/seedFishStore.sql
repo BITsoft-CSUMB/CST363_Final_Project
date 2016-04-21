702e65a1d0887b6be677a7069f31cacb3f1c0f61
Brittany Mazza <bmazza555@gmail.com>
2016-04-20 21:24:40 -0700

    WIP - Adding Seed Data
    Foreign key constraints are currently broken on all product insertions.

--- SQL_Scripts/seedFishStore.sql
+++ SQL_Scripts/seedFishStore.sql
@@ -1,141 +1,340 @@
+-- Echo back the responses from the following commands.
 set echo on
-
--- Add Employee Data
-
+-- ADD EMPLOYEE DATA --
 INSERT INTO employee
-VALUES('1001', '1 Hacker Way, Menlo Park, CA 94025', 'Brittany Mazza', '6505434800');
+VALUES ('1001', '1 Hacker Way, Menlo Park, CA 94025', 'Brittany Mazza', '6505434800');
 INSERT INTO employee
-VALUES('1002', '1600 Amphitheatre Parkway. Mountain View, CA 94043', 'John Lester', '6502530000');
+VALUES ('1002', '1600 Amphitheatre Parkway. Mountain View, CA 94043', 'John Lester', '6502530000');
 INSERT INTO employee
-VALUES('1003', '3500 Deer Creek Road, Palo Alto, CA 94304', 'Matthew Valancy', '6506815100');
+VALUES ('1003', '3500 Deer Creek Road, Palo Alto, CA 94304', 'Matthew Valancy', '6506815100');
 INSERT INTO employee
-VALUES('1004', '4040 Civic Center Dr # 300, San Rafael, CA 94903', 'Ashley Wallace', '4154990616');
+VALUES ('1004', '4040 Civic Center Dr # 300, San Rafael, CA 94903', 'Ashley Wallace', '4154990616');
+-- ADD CUSTOMER DATA --
+INSERT INTO customer
+VALUES ('12345', 'Noah Smith', '25898 Alabama Ct , Campbell CA 95008', '6505679664');
+INSERT INTO customer
+VALUES ('12346', 'Liam Johnson', '38180 Alaska Dr, Cupertino CA 95014', '6501041765');
+INSERT INTO customer
+VALUES ('12347', 'Jacob Williams', '28002 Arizona Cir, Gilroy CA 95020', '6507197782');
+INSERT INTO customer
+VALUES ('12348', 'Mason Jones', '96268 Arkansas Blvd, Los Altos CA 94022', '6506792368');
+INSERT INTO customer
+VALUES ('12349', 'William Brown', '40549 California St, Milpitas CA 95035', '6502696081');
+INSERT INTO customer
+VALUES ('12350', 'Ethan Davis', '27999 Colorado Ct , Monte Sereno CA 95030', '6501556933');
+INSERT INTO customer
+VALUES ('12351', 'Michael Miller', '11593 Connecticut Dr, Morgan Hill CA 95037', '6505489571');
+INSERT INTO customer
+VALUES ('12352', 'Alexander Wilson', '69920 Delaware Cir, Mountain View CA 94035', '6506419696');
+INSERT INTO customer
+VALUES ('12353', 'Jayden Moore', '7387 Florida Blvd, Palo Alto CA 94301', '6508014048');
+INSERT INTO customer
+VALUES ('12354', 'Daniel Taylor', '14660 Georgia St, San Jose CA 95150', '6502256364');
+INSERT INTO customer
+VALUES ('12355', 'Elijah Anderson', '53847 Hawaii Ct , Santa Clara CA 95050', '6506695316');
+INSERT INTO customer
+VALUES ('12356', 'Aiden Thomas', '45870 Idaho Dr, Saratoga CA 95070', '6507708345');
+INSERT INTO customer
+VALUES ('12357', 'James Jackson', '33428 Illinois Cir, Sunnyvale CA 94085', '6502613433');
+INSERT INTO customer
+VALUES ('12358', 'Benjamin White', '99496 Indiana Blvd, Los Gatos CA 95033', '6508496084');
+INSERT INTO customer
+VALUES ('12359', 'Matthew Harris', '71126 Iowa St, Los Altos Hills CA 94022', '6504843958');
+INSERT INTO customer
+VALUES ('12360', 'Emma Martin', '99610 Kansas Ct , Campbell CA 95008', '6509979209');
+INSERT INTO customer
+VALUES ('12361', 'Olivia Thompson', '97473 Kentucky Dr, Cupertino CA 95014', '6502856107');
+INSERT INTO customer
+VALUES ('12362', 'Isabella Garcia', '39767 Louisiana Cir, Gilroy CA 95020', '6508213684');
+INSERT INTO customer
+VALUES ('12363', 'Ava Martinez', '99342 Maine Blvd, Los Altos CA 94022', '6503409231');
+INSERT INTO customer
+VALUES ('12364', 'Mia Robinson', '28193 Maryland St, Milpitas CA 95035', '6502846913');
+INSERT INTO customer
+VALUES ('12365', 'Emily Clark', '12160 Massachusetts Ct , Monte Sereno CA 95030', '6508906597');
+INSERT INTO customer
+VALUES ('12366', 'Abigail Rodriguez', '37772 Michigan Dr, Morgan Hill CA 95037', '6502602128');
+INSERT INTO customer
+VALUES ('12367', 'Madison Lewis', '79415 Minnesota Cir, Mountain View CA 94035', '6501208682');
+INSERT INTO customer
+VALUES ('12368', 'Elizabeth Lee', '35764 Mississippi Blvd, Palo Alto CA 94301', '6504261594');
+INSERT INTO customer
+VALUES ('12369', 'Charlotte Walker', '2638 Missouri St, San Jose CA 95150', '6501114107');
+INSERT INTO customer
+VALUES ('12370', 'Avery Hall', '48930 Montana Ct , Santa Clara CA 95050', '6504766290');
+INSERT INTO customer
+VALUES ('12371', 'Sofia Allen', '31927 Nebraska Dr, Saratoga CA 95070', '6506971127');
+INSERT INTO customer
+VALUES ('12372', 'Chloe Hernandez', '36642 Nevada Cir, Sunnyvale CA 94085', '6508062863');
+INSERT INTO customer
+VALUES ('12373', 'Ella King', '47419 New Hampshire Blvd, Los Gatos CA 95033', '6503428401');
+INSERT INTO customer
+VALUES ('12374', 'Logan Wright', '10426 New Jersey St, Los Altos Hills CA 94022', '6502503260');
+INSERT INTO customer
+VALUES ('12375', 'Ameila Lopez', '75361 New Mexico Ct , Campbell CA 95008', '6508931654');
+INSERT INTO customer
+VALUES ('12376', 'David Hill', '1573 New York Dr, Cupertino CA 95014', '6508077610');
+INSERT INTO customer
+VALUES ('12377', 'Aubrey Scott', '58067 North Carolina Cir, Gilroy CA 95020', '6509386512');
+INSERT INTO customer
+VALUES ('12378', 'Anthony Green', '99226 North Dakota Blvd, Los Altos CA 94022', '6501124111');
+INSERT INTO customer
+VALUES ('12379', 'Addison Adams', '24020 Ohio St, Milpitas CA 95035', '6503536332');
+INSERT INTO customer
+VALUES ('12380', 'Joseph Baker', '90287 Oklahoma Ct , Monte Sereno CA 95030', '6507041441');
+INSERT INTO customer
+VALUES ('12381', 'Evelyn Gonzolex', '30519 Oregon Dr, Morgan Hill CA 95037', '6501685658');
+INSERT INTO customer
+VALUES ('12382', 'Joshua Nelson', '69453 Pennsylvania Cir, Mountain View CA 94035', '6509661810');
+INSERT INTO customer
+VALUES ('12383', 'Natalie Carter', '70883 Rhode Island Blvd, Palo Alto CA 94301', '6508369290');
+INSERT INTO customer
+VALUES ('12384', 'Andrew Mitchell', '20051 South Carolina St, San Jose CA 95150', '6505899673');
+INSERT INTO customer
+VALUES ('12385', 'Grace Perez', '81419 South Dakota Ct , Santa Clara CA 95050', '6505395790');
+INSERT INTO customer
+VALUES ('12386', 'Lucas Roberts', '31055 Tennessee Dr, Saratoga CA 95070', '6504479376');
+INSERT INTO customer
+VALUES ('12387', 'Hannah Turner', '50310 Texas Cir, Sunnyvale CA 94085', '6505302322');
+INSERT INTO customer
+VALUES ('12388', 'Gabriel Phillips', '68404 Utah Blvd, Los Gatos CA 95033', '6503382529');
+INSERT INTO customer
+VALUES ('12389', 'Zoe Campbell', '82544 Vermont St, Los Altos Hills CA 94022', '6509306734');
+INSERT INTO customer
+VALUES ('12390', 'Samuel Parker', '44179 Virginia Ct , Campbell CA 95008', '6501495726');
+INSERT INTO customer
+VALUES ('12391', 'Victoria Evans', '13456 Washington Dr, Cupertino CA 95014', '6504551723');
+INSERT INTO customer
+VALUES ('12392', 'Christopher Edwards', '57796 West Virginia Cir, Gilroy CA 95020', '6508028951');
+INSERT INTO customer
+VALUES ('12393', 'Lillian Collins', '92619 Wisconsin Blvd, Los Altos CA 94022', '6505248333');
+INSERT INTO customer
+VALUES ('12394', 'John Stewart', '76070 Wyoming St, Milpitas CA 95035', '6506013533');
+INSERT INTO customer
+VALUES ('12395', 'Lily Sanchez', '70502 Puerto Rico Ct , Monte Sereno CA 95030', '6501729459');
+INSERT INTO customer
+VALUES ('12396', 'Dylan Morris', '66481 Guam Dr, Morgan Hill CA 95037', '6509854539');
+INSERT INTO customer
+VALUES ('12397', 'Brooklyn Rogers', '58555 Simoa Cir, Mountain View CA 94035', '6501878479');
+INSERT INTO customer
+VALUES ('12398', 'Isaac Reed', '21863 Virgin Islands Blvd, Palo Alto CA 94301', '6506541041');
+INSERT INTO customer
+VALUES ('12399', 'Samantha Cook', '1970 Midway St, San Jose CA 95150', '6503322518');
+-- ADD VENDOR DATA --
+INSERT INTO vendor
+VALUES ('76543', 'Fish Food Supplier', '1063 Montgomery Ave, San Bruno, CA 94066');
+INSERT INTO vendor
+VALUES ('76544', 'Tanks R Us', '1495 ZEPHYR AVENUE, HAYWARD, CALIFORNIA 94544');
+INSERT INTO vendor
+VALUES ('76545', 'Water Plants', '2700 Mitchell Dr, Walnut Creek, CA 94598');
+INSERT INTO vendor
+VALUES ('76546', 'Aquarium Imporium', '301 Howard St #850, San Francisco, CA 94105');
+INSERT INTO vendor
+VALUES ('76547', 'Pan Ocean Aquarium', '2420 Camino Ramon #125, San Ramon, CA 94583');
+INSERT INTO vendor
+VALUES ('76548', 'A n M Aquatics', '601 California St, San Francisco, CA 94108');
+-- ADD PRODUCT CATEGORY DATA --
+INSERT INTO product_category 
+VALUES (1,'fish', 5);
+INSERT INTO product_category 
+VALUES (2,'fish food', 100);
+-- ADD PRODUCT DATA --
 
---Add Customer Data
-INSERT INTO customer
-VALUES('12345', 'Noah Smith', '25898 Alabama Ct , Campbell CA 95008', '6505679664');
-INSERT INTO customer
-VALUES('12346', 'Liam Johnson', '38180 Alaska Dr, Cupertino CA 95014', '6501041765');
-INSERT INTO customer
-VALUES('12347', 'Jacob Williams', '28002 Arizona Cir, Gilroy CA 95020', '6507197782');
-INSERT INTO customer
-VALUES('12348', 'Mason Jones', '96268 Arkansas Blvd, Los Altos CA 94022', '6506792368');
-INSERT INTO customer
-VALUES('12349', 'William Brown', '40549 California St, Milpitas CA 95035', '6502696081');
-INSERT INTO customer
-VALUES('12350', 'Ethan Davis', '27999 Colorado Ct , Monte Sereno CA 95030', '6501556933');
-INSERT INTO customer
-VALUES('12351', 'Michael Miller', '11593 Connecticut Dr, Morgan Hill CA 95037', '6505489571');
-INSERT INTO customer
-VALUES('12352', 'Alexander Wilson', '69920 Delaware Cir, Mountain View CA 94035', '6506419696');
-INSERT INTO customer
-VALUES('12353', 'Jayden Moore', '7387 Florida Blvd, Palo Alto CA 94301', '6508014048');
-INSERT INTO customer
-VALUES('12354', 'Daniel Taylor', '14660 Georgia St, San Jose CA 95150', '6502256364');
-INSERT INTO customer
-VALUES('12355', 'Elijah Anderson', '53847 Hawaii Ct , Santa Clara CA 95050', '6506695316');
-INSERT INTO customer
-VALUES('12356', 'Aiden Thomas', '45870 Idaho Dr, Saratoga CA 95070', '6507708345');
-INSERT INTO customer
-VALUES('12357', 'James Jackson', '33428 Illinois Cir, Sunnyvale CA 94085', '6502613433');
-INSERT INTO customer
-VALUES('12358', 'Benjamin White', '99496 Indiana Blvd, Los Gatos CA 95033', '6508496084');
-INSERT INTO customer
-VALUES('12359', 'Matthew Harris', '71126 Iowa St, Los Altos Hills CA 94022', '6504843958');
-INSERT INTO customer
-VALUES('12360', 'Emma Martin', '99610 Kansas Ct , Campbell CA 95008', '6509979209');
-INSERT INTO customer
-VALUES('12361', 'Olivia Thompson', '97473 Kentucky Dr, Cupertino CA 95014', '6502856107');
-INSERT INTO customer
-VALUES('12362', 'Isabella Garcia', '39767 Louisiana Cir, Gilroy CA 95020', '6508213684');
-INSERT INTO customer
-VALUES('12363', 'Ava Martinez', '99342 Maine Blvd, Los Altos CA 94022', '6503409231');
-INSERT INTO customer
-VALUES('12364', 'Mia Robinson', '28193 Maryland St, Milpitas CA 95035', '6502846913');
-INSERT INTO customer
-VALUES('12365', 'Emily Clark', '12160 Massachusetts Ct , Monte Sereno CA 95030', '6508906597');
-INSERT INTO customer
-VALUES('12366', 'Abigail Rodriguez', '37772 Michigan Dr, Morgan Hill CA 95037', '6502602128');
-INSERT INTO customer
-VALUES('12367', 'Madison Lewis', '79415 Minnesota Cir, Mountain View CA 94035', '6501208682');
-INSERT INTO customer
-VALUES('12368', 'Elizabeth Lee', '35764 Mississippi Blvd, Palo Alto CA 94301', '6504261594');
-INSERT INTO customer
-VALUES('12369', 'Charlotte Walker', '2638 Missouri St, San Jose CA 95150', '6501114107');
-INSERT INTO customer
-VALUES('12370', 'Avery Hall', '48930 Montana Ct , Santa Clara CA 95050', '6504766290');
-INSERT INTO customer
-VALUES('12371', 'Sofia Allen', '31927 Nebraska Dr, Saratoga CA 95070', '6506971127');
-INSERT INTO customer
-VALUES('12372', 'Chloe Hernandez', '36642 Nevada Cir, Sunnyvale CA 94085', '6508062863');
-INSERT INTO customer
-VALUES('12373', 'Ella King', '47419 New Hampshire Blvd, Los Gatos CA 95033', '6503428401');
-INSERT INTO customer
-VALUES('12374', 'Logan Wright', '10426 New Jersey St, Los Altos Hills CA 94022', '6502503260');
-INSERT INTO customer
-VALUES('12375', 'Ameila Lopez', '75361 New Mexico Ct , Campbell CA 95008', '6508931654');
-INSERT INTO customer
-VALUES('12376', 'David Hill', '1573 New York Dr, Cupertino CA 95014', '6508077610');
-INSERT INTO customer
-VALUES('12377', 'Aubrey Scott', '58067 North Carolina Cir, Gilroy CA 95020', '6509386512');
-INSERT INTO customer
-VALUES('12378', 'Anthony Green', '99226 North Dakota Blvd, Los Altos CA 94022', '6501124111');
-INSERT INTO customer
-VALUES('12379', 'Addison Adams', '24020 Ohio St, Milpitas CA 95035', '6503536332');
-INSERT INTO customer
-VALUES('12380', 'Joseph Baker', '90287 Oklahoma Ct , Monte Sereno CA 95030', '6507041441');
-INSERT INTO customer
-VALUES('12381', 'Evelyn Gonzolex', '30519 Oregon Dr, Morgan Hill CA 95037', '6501685658');
-INSERT INTO customer
-VALUES('12382', 'Joshua Nelson', '69453 Pennsylvania Cir, Mountain View CA 94035', '6509661810');
-INSERT INTO customer
-VALUES('12383', 'Natalie Carter', '70883 Rhode Island Blvd, Palo Alto CA 94301', '6508369290');
-INSERT INTO customer
-VALUES('12384', 'Andrew Mitchell', '20051 South Carolina St, San Jose CA 95150', '6505899673');
-INSERT INTO customer
-VALUES('12385', 'Grace Perez', '81419 South Dakota Ct , Santa Clara CA 95050', '6505395790');
-INSERT INTO customer
-VALUES('12386', 'Lucas Roberts', '31055 Tennessee Dr, Saratoga CA 95070', '6504479376');
-INSERT INTO customer
-VALUES('12387', 'Hannah Turner', '50310 Texas Cir, Sunnyvale CA 94085', '6505302322');
-INSERT INTO customer
-VALUES('12388', 'Gabriel Phillips', '68404 Utah Blvd, Los Gatos CA 95033', '6503382529');
-INSERT INTO customer
-VALUES('12389', 'Zoe Campbell', '82544 Vermont St, Los Altos Hills CA 94022', '6509306734');
-INSERT INTO customer
-VALUES('12390', 'Samuel Parker', '44179 Virginia Ct , Campbell CA 95008', '6501495726');
-INSERT INTO customer
-VALUES('12391', 'Victoria Evans', '13456 Washington Dr, Cupertino CA 95014', '6504551723');
-INSERT INTO customer
-VALUES('12392', 'Christopher Edwards', '57796 West Virginia Cir, Gilroy CA 95020', '6508028951');
-INSERT INTO customer
-VALUES('12393', 'Lillian Collins', '92619 Wisconsin Blvd, Los Altos CA 94022', '6505248333');
-INSERT INTO customer
-VALUES('12394', 'John Stewart', '76070 Wyoming St, Milpitas CA 95035', '6506013533');
-INSERT INTO customer
-VALUES('12395', 'Lily Sanchez', '70502 Puerto Rico Ct , Monte Sereno CA 95030', '6501729459');
-INSERT INTO customer
-VALUES('12396', 'Dylan Morris', '66481 Guam Dr, Morgan Hill CA 95037', '6509854539');
-INSERT INTO customer
-VALUES('12397', 'Brooklyn Rogers', '58555 Simoa Cir, Mountain View CA 94035', '6501878479');
-INSERT INTO customer
-VALUES('12398', 'Isaac Reed', '21863 Virgin Islands Blvd, Palo Alto CA 94301', '6506541041');
-INSERT INTO customer
-VALUES('12399', 'Samantha Cook', '1970 Midway St, San Jose CA 95150', '6503322518');
+/* FOR REFERENCE (to delete when done): 
+  product_id NUMBER(10) NOT NULL PRIMARY KEY,
+  name VARCHAR(50),
+  description VARCHAR(200),
+  unit_price NUMBER(5,2),
+  vendor_id NUMBER(10) REFERENCES vendor(vendor_id),
+  category_id NUMBER(10) NOT NULL REFERENCES product_category(category_id)); */
 
+INSERT INTO product 
+VALUES (
+  1,
+  'Beta',
+  'The Siamese fighting fish (Betta splendens), also sometimes colloquially known as the Betta, is a species in the Gourami family which is popular as an aquarium fish.',
+  0.50,
+  '76543',
+  1);
+INSERT INTO product 
+VALUES (
+  2,
+  'Goldfish',
+  'The goldfish (Carassius auratus) is a freshwater fish in the family Cyprinidae of order Cypriniformes. It was one of the earliest fish to be domesticated, and is one of the most commonly kept fish.',
+  0.50,
+  '76543',
+  1);
+INSERT INTO product 
+VALUES (
+  3,
+  'Achilles Tang',
+  'Acanthurus achilles, commonly known as Achilles tang or Achilles surgeonfish, is a tropical marine fish native to the Pacific Ocean.',
+  5.00,
+  78548,
+  1);
+INSERT INTO product 
+VALUES (
+  4,
+  'Angelfish',
+  'Pterophyllum is a small genus of freshwater fish from the family Cichlidae known to most aquarists as "angelfish".',
+  3.00,
+  '76543',
+  1);
+INSERT INTO product 
+VALUES (
+  5,
+  'Bloodfin Tetra',
+  'The bloodfin tetra (Aphyocharax anisitsi) is a species of characin from the Paraná River basin in South America.',
+  2.5,
+  '78548',
+  1);
+INSERT INTO product 
+VALUES (
+  6,
+  'Kaytee Kois Choice Premium Fish Food',
+  'Nutritionally complete and suitable for all seasons. Highly digestible for less waste and pond clarity. Made in the USA.',
+  16.99,
+  '76543',
+  2);
+INSERT INTO product 
+VALUES (
+  7,
+  'Tetra 16140 TetraFin Flakes, 7.06-Ounce',
+  'This Special Procure Formula Meets All Nutritional Requirements Of Cold-Water Fish And Will Stay Firm When Fish Strike.',
+  8.20,
+  '76543',
+  2);
+INSERT INTO product 
+VALUES (
+  8,
+  'Aqueon 06051 Betta Food, 0.95-Ounce',
+  'Balanced nutrition for daily feeding. Contains shrimp and other ingredients that bettas love. Perfect size to prevent overfeeding',
+  3.29,
+  '76543',
+  2);
+INSERT INTO product 
+VALUES (
+  9,
+  'TetraPond Koi Vibrance Food 2.5 Pound',
+  'Enhances reds and yellows. Floating soft sticks are easy to digest. Clear water formula',
+  12.99,
+  '76543',
+  2);
+-- ADD INVENTORY LOCATION DATA --
 
--- add vendor data
+/* FOR REFERENCE (to delete when done):
+  location_id NUMBER(10) NOT NULL PRIMARY KEY,
+  quantity NUMBER(5),
+  product_id NUMBER(10) REFERENCES product(product_id)); */
 
-INSERT INTO vendor
-VALUES('76543', 'Fish Food Supplier', '1063 Montgomery Ave, San Bruno, CA 94066');
-INSERT INTO vendor
-VALUES('76544', 'Tanks R Us', '1495 ZEPHYR AVENUE, HAYWARD, CALIFORNIA 94544');
-INSERT INTO vendor
-VALUES('76545', 'Water Plants', '2700 Mitchell Dr, Walnut Creek, CA 94598');
-INSERT INTO vendor
-VALUES('76546', 'Aquarium Imporium', '301 Howard St #850, San Francisco, CA 94105');
-INSERT INTO vendor
-VALUES('76547', 'Pan Ocean Aquarium', '2420 Camino Ramon #125, San Ramon, CA 94583');
-INSERT INTO vendor
-VALUES('76548', 'A&M Aquatics', '601 California St, San Francisco, CA 94108');
+/* 
+INSERT INTO inventory_location
+VALUES ();
+INSERT INTO inventory_location
+VALUES (); 
+*/
 
+-- ADD TANK LOG DATA --
+
+/* FOR REFERENCE (to delete when done):
+  location_id NUMBER(10) NOT NULL REFERENCES inventory_location(location_id),
+  read_date DATE,
+  temperature NUMBER(3),
+  ph NUMBER(2),
+  nitrate NUMBER(3),
+  ammonia NUMBER(2),
+*/
+
+/* 
+INSERT INTO tank_log
+VALUES ();
+INSERT INTO tank_log
+VALUES ();
+INSERT INTO tank_log
+VALUES ();
+INSERT INTO tank_log
+VALUES ();
+INSERT INTO tank_log
+VALUES ();
+INSERT INTO tank_log
+VALUES ();
+*/
+
+-- ADD SALES ORDER DATA --
+
+/* FOR REFERENCE (to delete when done):
+
+  so_num NUMBER(10) NOT NULL PRIMARY KEY,
+  so_date DATE,
+  customer_id NUMBER(10) REFERENCES customer(customer_id),
+  sales_rep_id NUMBER(10) REFERENCES employee(employee_id));
+
+*/
+
+/* 
+INSERT INTO sales_order
+VALUES ();
+INSERT INTO sales_order
+VALUES ();
+*/
+
+-- ADD SALES ORDER LINE DATA --
+
+/* FOR REFERENCE (to delete when done):
+
+  so_num NUMBER(10) NOT NULL REFERENCES sales_order(so_num),
+  line_num NUMBER(10) NOT NULL,
+  unit_price NUMBER(5,2),
+  quantity NUMBER(5),
+  product_id NUMBER(10) NOT NULL REFERENCES product(product_id),
+
+*/
+
+/* 
+INSERT INTO so_line
+VALUES ();
+INSERT INTO so_line
+VALUES ();
+INSERT INTO so_line
+VALUES ();
+INSERT INTO so_line
+VALUES ();
+INSERT INTO so_line
+VALUES ();
+*/
+
+-- ADD PRODUCT ORDER DATA --
+
+/* FOR REFERENCE (to delete when done):
+
+  po_num NUMBER(10) NOT NULL PRIMARY KEY,
+  po_date DATE,
+  vendor_id NUMBER(10) REFERENCES vendor(vendor_id),
+  entered_by NUMBER(10) REFERENCES employee(employee_id));
+
+*/
+
+/* 
+INSERT INTO product _order
+VALUES ();
+INSERT INTO product _order
+VALUES ();
+*/
+
+-- ADD PRODUCT ORDER LINE DATA --
+
+/* FOR REFERENCE (to delete when done):
+
+  po_num NUMBER(10) NOT NULL REFERENCES sales_order(so_num),
+  line_num NUMBER(10) NOT NULL,
+  quantity NUMBER(5),
+  product_id NUMBER(10) NOT NULL REFERENCES product(product_id),
+
+*/
+
+/* 
+INSERT INTO po_line
+VALUES ();
+INSERT INTO po_line
+VALUES ();
+*/
